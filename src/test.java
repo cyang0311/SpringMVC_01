35c16787d6cd0d3f6024dced60cd649ac7a700f7
cy <597498917@qq.com>
2019-07-16 00:24:21 +0800

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ViewResolver;
+
+public class test {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ web/main.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cy
+  Date: 2019/6/16
+  Time: 18:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Main</title>
+</head>
+<body>
+
+</body>
+</html>
